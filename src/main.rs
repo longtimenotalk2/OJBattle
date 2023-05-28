c1dd2da8fd0557e8cc5dd7ba1dd6dd9d91953988
lhw <lihaiwei19921122@163.com>
2023-05-28 14:45:50 +0800

    simplest hello world

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,10 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+
+// hint: "http://127.0.0.1:8080/"
 
 #[get("/")]
 async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
-}
-
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
-
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
+    HttpResponse::Ok().body("Hello world!!!!!")
 }
 
 #[actix_web::main]
@@ -19,8 +12,6 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
