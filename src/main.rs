742255e2297c52f46e0ebbf6123adbab4dba669e
longtimenotalk2 <55969093+longtimenotalk2@users.noreply.github.com>
2023-05-30 00:37:21 +0800

    Update main.rs : -p port

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,33 @@
-//http://127.0.0.1:8080/battle/4/1/1/-1/4/0/0/1
+//git push -u origin main
+
+
+//cargo run -- -p [port]
+//http://127.0.0.1:[port]/battle/4/1/1/-1/4/0/0/1
+
+
 
 pub mod oj;
+use crate::oj::main_battle;
 
+// fn main() {
+//     println!("{}", main_battle(
+//         5,
+//         -1,
+//         -1,
+//         2,
+//         5,
+//         -1,
+//         -1,
+//         2,
+//     ))
+// }
+
+
+use std::env;
 use actix_web::{get, web, Result};
 use serde::Deserialize;
 
-use crate::oj::main_battle;
+
 
 #[derive(Deserialize)]
 struct Info {
@@ -28,10 +50,19 @@ async fn index(info: web::Path<Info>) -> Result<String> {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+
+    let mut port_str = String::from("8080");
+    for i in 1..args.len() {
+        if args[i] == "-p" {
+            port_str = String::from(&args[i + 1]);
+        }
+    }
+    let port : u16 = port_str.parse().unwrap();
     use actix_web::{App, HttpServer};
 
     HttpServer::new(|| App::new().service(index))
-        .bind(("127.0.0.1", 8080))?
+        .bind(("0.0.0.0", port))?
         .run()
         .await
 }
